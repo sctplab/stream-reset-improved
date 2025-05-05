ace6b5b93e8f2cefb6b1ae9bfcb882fcba02c33c
Michael Tuexen <tuexen@fh-muenster.de>
2025-05-05 22:52:14 +0200

    Fix a bug introduced by one of the last commits.

--- netinet/sctp_pcb.c
+++ netinet/sctp_pcb.c
@@ -513,7 +513,11 @@ sctp_add_addr_to_vrf(uint32_t vrf_id, void *ifn, uint32_t ifn_index,
 					SCTPDBG(SCTP_DEBUG_PCB4,
 					"Clearing deleted ifa flag\n");
 					sctp_ifap->localifa_flags = SCTP_ADDR_VALID;
-					sctp_gather_internal_ifa_flags(sctp_ifap);
+#ifdef INET6
+					if (sctp_ifap->address.sa.sa_family == AF_INET6) {
+						sctp_gather_internal_ifa_flags(sctp_ifap);
+					}
+#endif
 					sctp_ifap->ifn_p = sctp_ifnp;
 					atomic_add_int(&sctp_ifap->ifn_p->refcount, 1);
 				}
@@ -536,7 +540,11 @@ sctp_add_addr_to_vrf(uint32_t vrf_id, void *ifn, uint32_t ifn_index,
 		} else {
 			/* Repair ifn_p, which was NULL... */
 			sctp_ifap->localifa_flags = SCTP_ADDR_VALID;
-			sctp_gather_internal_ifa_flags(sctp_ifap);
+#ifdef INET6
+			if (sctp_ifap->address.sa.sa_family == AF_INET6) {
+				sctp_gather_internal_ifa_flags(sctp_ifap);
+			}
+#endif
 			SCTPDBG(SCTP_DEBUG_PCB4,
 			    "Repairing ifn %p for ifa %p\n",
 			    (void *)sctp_ifnp, (void *)sctp_ifap);
@@ -584,7 +592,12 @@ sctp_add_addr_to_vrf(uint32_t vrf_id, void *ifn, uint32_t ifn_index,
 	}
 #endif
 	sctp_ifap->localifa_flags = SCTP_ADDR_VALID | SCTP_ADDR_DEFER_USE;
-	sctp_gather_internal_ifa_flags(sctp_ifap);
+	sctp_ifap->flags = ifa_flags;
+#ifdef INET6
+	if (addr->sa_family == AF_INET6) {
+		sctp_gather_internal_ifa_flags(sctp_ifap);
+	}
+#endif
 	/* Set scope */
 	switch (sctp_ifap->address.sa.sa_family) {
 #ifdef INET
